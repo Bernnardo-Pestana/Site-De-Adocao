64a76ad67347825e9635e70d1cc08ec6a9a400e4
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-01 13:41:06 -0300

    init

--- index.js
+++ index.js
@@ -143,8 +143,6 @@ app.post("/salvar_adocao",(req,res)=>{
 
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
