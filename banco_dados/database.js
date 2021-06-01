8f4fa22baf27f8258643f8ab7b47ad3114724e7c
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-01 14:07:08 -0300

    init

--- banco_dados/database.js
+++ banco_dados/database.js
@@ -1,10 +1,6 @@
 const Sequelize = require('sequelize');
 
-<<<<<<< HEAD
 const connection = new Sequelize('site_adocao','root','Txai1990', //nome do banco de dados, usuario e senha
-=======
-const connection = new Sequelize('usuarios','root','Txai1990', //nome do banco de dados, usuario e senha
->>>>>>> fa8ac051c1aa72e8113dfe05257f24d6fbc8ae26
   {
     host: 'localhost', //servidor
     dialect: 'mysql'//tipo de banco de dado
