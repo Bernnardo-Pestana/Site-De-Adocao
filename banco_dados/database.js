6c9916cd2ef75af70cc6309b37d85f86288fd90c
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-02 12:50:07 -0300

    init

--- banco_dados/database.js
+++ banco_dados/database.js
@@ -2,10 +2,21 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize({
   connectionString: process.env.DATABASE_URL,
+  dialect: 'postgres',
   ssl: {
     rejectUnauthorized: false
-  },
-  dialect: 'postgres'
+  }
+  
+});
+
+connection.connect();
+
+connection.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  connection.end();
 });
 
   module.exports = connection; //exporta a conexao do servidor
