ee7b43e816f70fe13c096f1b5e8662f36fc2f071
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-02 12:19:33 -0300

    init

--- banco_dados/database.js
+++ banco_dados/database.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('site_adocao','root','Txai1990', //nome do banco de dados, usuario e senha
-  {
-    host: 'localhost', //servidor
-    dialect: 'mysql'//tipo de banco de dado
-  });
+const connection = new Sequelize({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
   module.exports = connection; //exporta a conexao do servidor
