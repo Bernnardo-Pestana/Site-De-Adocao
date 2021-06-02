d1534008731000060213cf0fb1afd79928cda2b5
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-02 12:24:25 -0300

    init

--- banco_dados/database.js
+++ banco_dados/database.js
@@ -4,7 +4,8 @@ const connection = new Sequelize({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  dialect: 'postgres'
 });
 
   module.exports = connection; //exporta a conexao do servidor
