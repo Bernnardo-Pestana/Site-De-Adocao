01bb8db07b1f6144508ef21f74081eb232cb1f3e
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-02 13:18:57 -0300

    init

--- banco_dados/database.js
+++ banco_dados/database.js
@@ -9,14 +9,4 @@ const connection = new Sequelize({
   
 });
 
-
-
-connection.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  connection.end();
-});
-
   module.exports = connection; //exporta a conexao do servidor
