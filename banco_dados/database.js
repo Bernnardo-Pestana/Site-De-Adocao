cab6620fcaccaf3f873b62454faeddab795c52f8
Bernnardo-Pestana <73316772+Bernnardo-Pestana@users.noreply.github.com>
2021-06-02 17:24:20 -0300

    int

--- banco_dados/database.js
+++ banco_dados/database.js
@@ -9,4 +9,24 @@ const connection = new Sequelize({
   
 });
 
-  module.exports = connection; //exporta a conexao do servidor
+//  module.exports = connection; //exporta a conexao do servidor
+
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+module.exports = client ;
